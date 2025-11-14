27b75d415343c93374e79cfae1daf77821348999
Júlio Cesar de Oliveira Alves <20231144010043@ifrn.local>
2025-11-14 14:36:08 -0300

    Júlio: Adição de anotações sobre o script das rotas Express no BACKEND

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -13,14 +13,17 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Como avmos usar o Express só para API. podemos apagar a pasta views e os scripts que definem as rotas das paginas views
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//app.use rota em agrupamento, ou seja, as rotas que estão na página são lidas
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- BACKEND/routes/index.js
+++ BACKEND/routes/index.js
@@ -3,7 +3,12 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Olá mundo' });
+  res.render('index', { title: 'mundo' });
+});
+
+router.get('/dados', function(req, res){
+  let msg = {"mundo":"ola"};
+  res.send(msg);
 });
 
 module.exports = router;
