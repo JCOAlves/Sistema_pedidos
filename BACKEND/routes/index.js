c941609ca46a2ce0e5359a39978b0069ae1b3a08
Júlio César <o.cesar@escolar.ifrn.edu.br>
2025-11-17 21:40:12 +0000

    Júlio: Instalação do módulo MySQL, criação da pasta utills/ no BACKEND e adição de notas

--- BACKEND/routes/index.js
+++ BACKEND/routes/index.js
@@ -2,13 +2,17 @@ let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'mundo' });
+router.get('/index/:nome/:sobrenome', function(req, res, next) {
+  res.render('index', { title: req.params.nome+" "+req.params.sobrenome });
 });
 
-router.get('/dados', function(req, res){
-  let msg = {"mundo":"ola"};
-  res.send(msg);
+router.get('/notas', function(req, res){
+  let nota1 = Number(req.query.nota1);
+  let nota2 = Number(req.query.nota2);
+  let nota3 = Number(req.query.nota3);
+  const media = ((nota1+nota2+nota3)/3).toFixed(0);
+  const mensagem = media >= 60 ? `Sua média é ${media}.\nParabens! Você foi aprovado!` : `Sua média é ${media}.\nInfelizmente você foi reprovado.`;
+  res.send(mensagem);
 });
 
 module.exports = router;
---
+++ BACKEND/utills/Nota_db.md
@@ -0,0 +1,4 @@
+# Dados da conexão do Banco de Dados
+Por motivos de segurança, os dados da conexão com o banco, como usuario e senha, não devem ser commitados. Os dados ficaram armazenados em variaveis chave-valor no arquivo ```.env```, a qual cada um deve criar um para o seu repositorio local, visto que o ```.env``` também vai ser ignorado no commit.
+
+Assinado: Júlio
